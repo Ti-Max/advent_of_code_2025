60420a7c5a8ea03bd791ae78da271d8c7f5dd497
Timofei Maksimov <maxtimofy@gmail.com>
2025-12-02 20:16:47 +0100

    improve(day 1): use rem_euclid, iter:scan, iter::fold, iter::map

--- 1/src/dial_rotator_v2.rs
+++ 1/src/dial_rotator_v2.rs
@@ -16,33 +16,29 @@ fn number_after_first_char(line: &str) -> i32 {
 }
 
 pub fn rotate_and_count_zeros(lines: &Vec<&str>) -> i32 {
-    let mut position = INITIAL_VALUE;
-    let mut zero_count = 0;
-    for line in lines {
-        let direction: char = get_first_char(line);
-        let count: i32 = number_after_first_char(line);
+    lines
+        .iter()
+        .scan(INITIAL_VALUE, |state: &mut i32, line| {
+            let direction: char = get_first_char(line);
+            let count: i32 = number_after_first_char(line);
 
-        match direction {
-            'R' => position += count,
-            'L' => position -= count,
-            _ => panic!("the first character of the line is either R nor L"),
-        }
-
-        position = position.rem_euclid(VALUE_COUNT);
-
-        if position == 0 {
-            zero_count += 1;
-        }
-    }
-
-    zero_count
+            match direction {
+                'R' => *state += count,
+                'L' => *state -= count,
+                _ => panic!("the first character of the line is either R nor L"),
+            }
+            Some(*state)
+        })
+        .map(|state| state.rem_euclid(VALUE_COUNT))
+        .fold(0, |acc, v| if v == 0 { acc + 1 } else { acc })
 }
 
 fn count_zero_ticks(position: &i32) -> i32 {
     position / VALUE_COUNT
 }
 
-pub fn rotate_and_count_all_zeros(lines: &Vec<&str>) -> i32 {
+// same as v1
+pub fn rotate_and_count_all_zeros(lines: Vec<&str>) -> i32 {
     let mut position = INITIAL_VALUE;
     let mut zero_count = 0;
     for line in lines {
--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,12 +1,22 @@
 use std::fs;
 mod dial_rotator;
+mod dial_rotator_v2;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = fs::read_to_string("data/input")?;
     let lines: Vec<&str> = file.lines().collect();
 
     let first_answer = dial_rotator::rotate_and_count_zeros(&lines);
-    let second_answer = dial_rotator::rotate_and_count_all_zeros(lines);
+    let second_answer = dial_rotator::rotate_and_count_all_zeros(&lines);
+
+    println!("First answer: {}", first_answer);
+    println!("Second answer: {}", second_answer);
+
+    println!("------");
+    println!("V2");
+    println!("------");
+    let first_answer = dial_rotator_v2::rotate_and_count_zeros(&lines);
+    let second_answer = dial_rotator_v2::rotate_and_count_all_zeros(lines);
 
     println!("First answer: {}", first_answer);
     println!("Second answer: {}", second_answer);
