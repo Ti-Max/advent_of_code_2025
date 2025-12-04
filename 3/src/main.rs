aaf9985fc9a4220529a3c41d9028d1c32ca3313b
Timofei Maksimov <maxtimofy@gmail.com>
2025-12-04 01:10:28 +0100

    day 3 - ⭐⭐

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,48 +1,61 @@
-use std::{fs, ops::RangeInclusive};
+use std::{fs, usize};
 
-fn is_invalid_v1(id: i64) -> bool {
-    let str = id.to_string();
-    if str.len().is_multiple_of(2) {
-        let middle = str.len() / 2;
-        str[..middle] == str[middle..]
-    } else {
-        false
-    }
+fn find_largest_number_v1(line: &str) -> u64 {
+    // reverse to get the first maximum digit
+    let (index_from_end, first_digit) = line
+        .chars()
+        .rev()
+        .enumerate()
+        .skip(1)
+        .max_by_key(|(_i, d)| *d)
+        .expect("line should not be empty");
+
+    let second_digit = line
+        .chars()
+        .skip(line.len() - index_from_end)
+        .max()
+        .expect("line shold contain at least two digits");
+
+    [first_digit, second_digit]
+        .iter()
+        .collect::<String>()
+        .parse()
+        .unwrap()
 }
 
-fn is_invalid_v2(id: i64) -> bool {
-    let id = id.to_string();
-    // split the string into all possible chunks
-    (2..=id.len()).any(|i| {
-        if id.len().is_multiple_of(i) {
-            id.as_bytes()
-                .chunks(id.len() / i)
-                .map(|c| -> &str { std::str::from_utf8(c).unwrap() })
-                .collect::<Vec<&str>>()
-                .as_slice()
-                .windows(2)
-                .all(|w| w[0] == w[1])
-        } else {
-            false
-        }
-    })
-}
+const BATTERIES_COUNT: usize = 12;
 
-fn all_ids(s: &str) -> impl Iterator<Item = i64> {
-    s.trim()
-        .split(',')
-        .map(|s| s.split_once('-').unwrap())
-        .map(|(start, end)| -> RangeInclusive<i64> {
-            start.parse().unwrap()..=end.parse().unwrap()
+fn find_largest_number_v2(line: &str) -> u64 {
+    // reverse to get the first maximum digit
+
+    (0..BATTERIES_COUNT)
+        .scan(0, |state, i| {
+            let (index_from_end, digit) = line
+                .chars()
+                .skip(*state)
+                // can't do rev() because "the trait bound is not satisfied bla bla bla"
+                .collect::<String>()
+                .chars()
+                .rev()
+                .enumerate()
+                .skip(BATTERIES_COUNT - i - 1)
+                .max_by_key(|(_i, d)| *d)
+                .unwrap();
+
+            *state = line.len() - index_from_end;
+
+            Some(digit)
         })
-        .flat_map(|range| -> Vec<i64> { range.collect() })
+        .collect::<String>()
+        .parse()
+        .unwrap()
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let string = fs::read_to_string("data/input")?;
+    let file = fs::read_to_string("data/input")?;
 
-    let first_answer: i64 = all_ids(&string).filter(|id| is_invalid_v1(*id)).sum();
-    let second_answer: i64 = all_ids(&string).filter(|id| is_invalid_v2(*id)).sum();
+    let first_answer: u64 = file.lines().map(|line| find_largest_number_v1(line)).sum();
+    let second_answer: u64 = file.lines().map(|line| find_largest_number_v2(line)).sum();
 
     println!("First answer: {}", first_answer);
     println!("Second answer: {}", second_answer);
